ad5fe83cd1a0278b091899ffdd056c74d57c6a5e
mitchballe <mitchballe@msn.com>
2016-05-28 06:21:50 -0600

    Fixed Play Pause Button

--- ISL29125SimpleM6/ISL29125SimpleM6.ino
+++ ISL29125SimpleM6/ISL29125SimpleM6.ino
@@ -1,167 +1,163 @@
-/******************************************************************************
-ISL29125_interrupts.ino
-Example demonstrating use of the ISL29125 RGB sensor library with interrupts.
-Jordan McConnell @ SparkFun Electronics
-18 Apr 2014
-https://github.com/sparkfun/SparkFun_ISL29125_Breakout_Arduino_Library
-
-This example shows how to use the ISL29125 sensor using interrupts. It
-demonstrates how to make a more advanced configuration to set this up that goes
-beyond the basics needed simply to acquire readings from the sensor. Interrupts
-are triggered when sensor readings are above or below set thresholds and this
-example also shows how to set those up. It also teaches how to read the sensor
-after the interrupt and how to read/clear status flags so another interrupt can
-be triggered and so you can interpret the status of the sensor. If you plan on
-using this sensor in an interrupt driven project, this is the example for you.
-
-Developed/Tested with:
-Arduino Uno
-Arduino IDE 1.0.5
-
-Requires:
-SparkFun_ISL29125_Arduino_Library
-
-This code is beerware.
-Distributed as-is; no warranty is given. 
-******************************************************************************/
-
+//include the following libraries
 #include <Wire.h>
 #include <SparkFunISL29125.h>
 #include <CountUpDownTimer.h>
-#include <SuperChrono.h>
+#include <Bounce2.h>
+//define what i/o pin is associated with which button
+const int PlayPauseButton = 4;
+const int MinusRedScoreButton = 5;
+const int MinusGreenScoreButton = 6;
+const int MinusTimeButton = 7;
+
 CountUpDownTimer T(UP);
 
 // Declare sensor object
 SFE_ISL29125 RGB_sensor;
 
+//initiate bounce object for each button
+Bounce PlayPause = Bounce();
+Bounce MinusRedScore = Bounce();
+Bounce MinusGreenScore = Bounce();
+Bounce MinusTime = Bounce();
+
 // The red score
 unsigned int redScore = 0;
 
+boolean playPauseStatus = true;
+boolean gameStatus = false;
+
 // The green score
-unsigned int greenScore=0;
+unsigned int greenScore = 0;
 
 // the winning score
-unsigned int winScore=5;
- 
-//the pin where the button is connected
-const int BUTTON=7;
+const unsigned int WINSCORE = 5;
+
+// integer used to store hit status
+int hitStatus = 0;
 
 //value used to store the state of the button pin
-int buttonStatus=HIGH;
-
-//variable to store the old button pin state
-int old_buttonStatus=HIGH;
-
-//varialble to store state of button
-int state=0;
-
-//variable to store pause time
-unsigned long pTime=0.0;
+int buttonStatus = HIGH;
 
 //variable to store max play time
-unsigned long maxTime=60;
+const unsigned MAXTIME = 60;
 
-long unsigned int red_value = 0; // Stores sensor reading for red light intensity
-long unsigned int redMax=0x13E5; //threshold for recording a read hit
-long unsigned int green_value=0; // Stores sensor reading for green light intensity
-long unsigned int greenMax=0x5200; //threshold for recording a green hit
+//variable to store time of previous time check
+unsigned lastTime = 0;
+
+long unsigned int redValue = 0; // Stores sensor reading for red light intensity
+long unsigned int REDTRIGGER = 0x1000; //threshold for recording a red hit
+long unsigned int greenValue = 0; // Stores sensor reading for green light intensity
+long unsigned int GREENTRIGGER = 0x3000; //threshold for recording a green hit
 
 //function to show time
-void elapseTime(){
-   Serial.print("Time: ");
-   Serial.print(T.ShowMinutes());
-   Serial.print(":");
-   Serial.println(T.ShowSeconds());
+void elapseTime() {
+  Serial.print("Time: ");
+  Serial.print(T.ShowMinutes());
+  Serial.print(":");
+  Serial.println(T.ShowSeconds());
 }
 
 //function to show score
 void score() {
-   Serial.print("Red Score: ");
-   Serial.println(redScore);
-   Serial.print("Green Score: ");
-   Serial.println(greenScore);
+  Serial.print("Red Score: ");
+  Serial.println(redScore);
+  Serial.print("Green Score: ");
+  Serial.println(greenScore);
 }
 
 //function to exectute when game is over
-void gameOver(){
-   Serial.println("GAME OVER");
-   score();
-   elapseTime();
-   Serial.println("HIT BUTTON TO START NEW GAME"); 
-   }
-
-// function to restart the game
-void newGame(){
-          buttonDebounce();
-          redScore=0;
-          greenScore=0;
-          T.ResetTimer();
-          return;
-          }
-          
-//function to check score and elapsed time
-
-void checkScore(){
-  if((greenScore==winScore)||(redScore==winScore)){ //check score
-      gameOver();
-      newGame();
-      }
-    else {
-        continueGame();
-         } 
+void gameOver() {
+  Serial.println("GAME OVER");
+  score(); //print score
+  elapseTime(); //print time
+  redScore = 0;
+  greenScore = 0;
+  T.ResetTimer();
+  Serial.println("HIT BUTTON TO START NEW GAME");
+  buttonControl();
+  T.ResumeTimer();
 }
 
-void checkTime(){
-  if(T.ShowTotalSeconds()>=maxTime){ //check game elapsed time
-      gameOver();
-      newGame();
-      }
- }
+// function to restart the game
+void newGame() {
+  redScore = 0;
+  greenScore = 0;
+  T.ResetTimer();
+  buttonControl();
+  T.ResumeTimer();
+}
+
+//function to check score and elapsed time
+boolean checkScore() {
+  if ((greenScore == WINSCORE) || (redScore == WINSCORE)) {
+    return (true);
+  }
+  else {
+    return (false);
+  }
+}
+
+boolean checkTime() {
+  if (T.ShowTotalSeconds() >= MAXTIME) {
+    return (true);
+  }
+  else {
+    return (false);
+  }
+}
 
 // function to detect the button push and avoid bouncing
-void buttonDebounce(){
-    while (state==0) {
-          //Serial.println("button not pushed");
-        buttonStatus=digitalRead(BUTTON); // read input value and store it 
-        if((buttonStatus==LOW)&& (old_buttonStatus==HIGH)){ // check if button has been pushed (LOW)  or not HIGH
-            state=1-state; //
-            delay(50); //delay for 50 milliseconds
-          }
-        old_buttonStatus=buttonStatus;                     
-          }
-        state=0; // Change the state back to zero for next button push
-          }
-  
-  // function to continue game after button is hit
-  void continueGame(){
-          score();
-          elapseTime();
-          Serial.println("HIT BUTTON TO CONTINUE GAME"); 
-          buttonDebounce();
-          T.ResumeTimer();
-          delay(250);
-          }
+void buttonControl() {
+  while (playPauseStatus == false) {
+    PlayPause.update();
+    playPauseStatus = PlayPause.fell();
+  }
+  playPauseStatus = false;
+}
+
+
+// function to continue game after button is hit
+void continueGame() {
+  score(); //print score
+  elapseTime(); //print time
+  Serial.println("HIT BUTTON TO CONTINUE GAME");
+  buttonControl();
+  T.ResumeTimer();
+}
 
 // Set up serial communication, initialize the sensor, and set up interrupts
-void setup()
-{
+void setup() {
   // set the time
- T.SetTimer(0,0,0); //set the run start time
- 
- T.StartTimer(); //start the timer
+  T.SetTimer(0, 0, 0); //set the run start time
+
+  T.StartTimer(); //start the timer
 
   // Initialize serial communication
   Serial.begin(115200);
 
-  //tell Arduino that BUTTON is an input
-  pinMode(BUTTON,INPUT_PULLUP);  // internal pull-up
+  //set up bottons with internal pull-up
+  pinMode(PlayPauseButton, INPUT_PULLUP);
+  pinMode(MinusRedScoreButton, INPUT_PULLUP);
+  pinMode(MinusGreenScoreButton, INPUT_PULLUP);
+  pinMode(MinusTimeButton, INPUT_PULLUP);
 
-  // Initialize the ISL29125 and verify its presence
-  if (RGB_sensor.init())
-  {
+  //set up bounce instance for each botton
+  PlayPause.attach(PlayPauseButton);
+  PlayPause.interval(10); //interval in ms
+  MinusRedScore.attach(MinusRedScoreButton);
+  MinusRedScore.interval(5); //interval in ms
+  MinusGreenScore.attach(MinusGreenScoreButton);
+  MinusGreenScore.interval(5); //interval in ms
+  MinusTime.attach(MinusTimeButton);
+  MinusTime.interval(5); //interval in ms
+
+  //Initialize the ISL29125 and verify its presence
+  if (RGB_sensor.init()) {
     Serial.println("Sensor Initialization Successful\n\r");
   }
-  
+  if (!(RGB_sensor.init())) {
+    Serial.println("Sensor Initialization Not Successful\n\r");
+  }
   // Advanced configuration: Interrupts based solely on red light intensity. ~100ms per sensor reading.
   // Config1: CFG1_MODE_RG - only read red and green
   //          CFG1_10KLUX - 10K Lux is full light scale
@@ -169,38 +165,57 @@ void setup()
   // Config3: CFG3_R_INT - trigger interrupts based on sensor readings for red light intensity
   //          CFG3_INT_PRST8 - only trigger interrupt if red sensor reading crosses threshold 8 consecutive times
   // For other configuration options, look at the SFE_ISL29125.h file in the SFE_ISL29125_Library folder
-  // RGB_sensor.config(CFG1_MODE_RG | CFG1_10KLUX, CFG2_IR_ADJUST_HIGH, CFG3_NO_INT | CFG3_INT_PRST8);
- }
- 
+  //RGB_sensor.config(CFG1_MODE_RG | CFG1_10KLUX | CFG1_12BIT | CFG1_ADC_SYNC_TO_INT, CFG2_IR_OFFSET_ON | CFG2_IR_ADJUST_HIGH, CFG3_NO_INT);
+}
 
 void loop()
-{ 
+{
   T.Timer(); // run the timer
-  elapseTime();
+  if (T.TimeHasChanged() ) { // this prevents the time from being constantly shown.
+    elapseTime();
+  }
   // Read the detected light intensity of the red and green visible spectrum
-    red_value = RGB_sensor.readRed();
-    green_value = RGB_sensor.readGreen();
-  
-    if ((red_value>redMax)&&!(green_value>greenMax)) //check if red threshold is exceeded and green threshold is not
-    {
-        T.PauseTimer(); // pause timer if red or green threshold is exceeded
-        redScore++; // increment hit counter for red
-        checkScore(); //check score and time
-           }
-    else if ((green_value>greenMax)&&!(red_value>redMax)) //check if green threshold is exceeded and red threshold is not
-    {
-        T.PauseTimer(); // pause timer if red or green threshold is exceeded
-        greenScore++; // increment hit counter for green
-        checkScore(); //check score and time
-          }
-    else if ((green_value>greenMax)&&(red_value>redMax)) //check if both thresholds are exceeded
-    {
-        T.PauseTimer(); // pause timer if red or green threshold is exceeded
-        redScore++; // increment hit counter for red
-        greenScore++; // increment hit counter for red
-        checkScore(); //check score and time
-           }
-    checkTime();
-       }
+  redValue = RGB_sensor.readRed();
+  greenValue = RGB_sensor.readGreen();
+  hitStatus = (greenValue > GREENTRIGGER) * 2 + (redValue > REDTRIGGER);
+  switch (hitStatus) {
+    case 1: // only red scores a hit
+      redScore++; // increment hit counter for red
+      break;
+    case 2: // only green scores a hit
+      greenScore++; // increment hit counter for green
+      break;
+    case 3:  // both grean and red score hits
+      redScore++; // increment hit counter for red
+      greenScore++; // increment hit counter for red
+      break;
+  }
+  if (hitStatus != 0 && not((greenScore == WINSCORE) || (redScore == WINSCORE))) {
+    T.PauseTimer(); // pause timer
+    score(); //print score
+    elapseTime(); //print time
+    hitStatus = 0;
+    Serial.println("HIT BUTTON TO CONTINUE GAME");
+    playPauseStatus = false;
+    buttonControl();
+    T.ResumeTimer();
+  }
+  if ((T.ShowTotalSeconds() >= MAXTIME) || ((greenScore == WINSCORE) || (redScore == WINSCORE))) {
+    T.PauseTimer(); // pause timer
+    Serial.println("GAME OVER");
+    score(); //print score
+    elapseTime(); //print time
+    redScore = 0;
+    greenScore = 0;
+    hitStatus = 0;
+    T.ResetTimer();
+    Serial.println("HIT BUTTON TO START NEW GAME");
+    playPauseStatus = false;
+    buttonControl();
+    T.ResumeTimer();
+  }
+}
+
+
 
 
